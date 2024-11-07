0af19347e1a25c7b73c501a0f0c5913f0762a6ef
chuongvo19 <vochuong193@gmail.com>
2024-11-08 00:45:40 +0700

    migration table bookings

---
+++ entry_exam/database/migrations/2024_11_07_173624_create_bookings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id('booking_id');
+            $table->foreignId('hotel_id')->constrained('hotels', 'hotel_id')->onDelete('cascade');
+            $table->string('customer_name', 255);
+            $table->string('customer_contact', 255);
+            $table->timestamp('checkin_time');
+            $table->timestamp('checkout_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
